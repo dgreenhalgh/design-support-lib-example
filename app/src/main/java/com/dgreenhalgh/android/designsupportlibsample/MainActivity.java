08d349255efed3e9214b731840f58883f4b5d697
David Greenhalgh <davidrgreenhalgh@gmail.com>
2015-09-01 16:12:43 -0400

    Added TabLayout to MainActvity

--- app/src/main/java/com/dgreenhalgh/android/designsupportlibsample/MainActivity.java
+++ app/src/main/java/com/dgreenhalgh/android/designsupportlibsample/MainActivity.java
@@ -1,37 +1,25 @@
 package com.dgreenhalgh.android.designsupportlibsample;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.activity_main_tabLayout) TabLayout mTabLayout;
+    @Bind(R.id.activity_main_viewPager) ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        ButterKnife.bind(this);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        mViewPager.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager()));
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 }
---
+++ app/src/main/java/com/dgreenhalgh/android/designsupportlibsample/PageFragment.java
@@ -0,0 +1,48 @@
+package com.dgreenhalgh.android.designsupportlibsample;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+
+public class PageFragment extends Fragment {
+
+    private static final String ARG_PAGE_INDEX = "PAGE_INDEX";
+
+    @Bind(R.id.fragment_page_index_label) TextView mIndexLabelTextView;
+
+    private int mPageIndex;
+
+    public static Fragment newInstance(int pageIndex) {
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE_INDEX, pageIndex);
+        Fragment fragment = new PageFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPageIndex = getArguments().getInt(ARG_PAGE_INDEX);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_page, container, false);
+        ButterKnife.bind(this, view);
+
+        String pageLabel = getString(R.string.fragment_page_index_label, mPageIndex + 1);
+        mIndexLabelTextView.setText(pageLabel);
+
+        return view;
+    }
+}
