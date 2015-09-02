9aadfb0a453f6d9573994cf84cb7f74c4aa08e0c
David Greenhalgh <davidrgreenhalgh@gmail.com>
2015-09-02 13:20:58 -0400

    Added nav drawer with NavigationView to new sample

--- new/app/src/main/java/com/dgreenhalgh/android/designsupportlibsample/MainActivity.java
+++ new/app/src/main/java/com/dgreenhalgh/android/designsupportlibsample/MainActivity.java
@@ -3,17 +3,23 @@ package com.dgreenhalgh.android.designsupportlibsample;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ViewPager mViewPager;
+    private Toolbar mToolbar;
     private TabLayout mTabLayout;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(mToolbar);
 
         mViewPager = (ViewPager) findViewById(R.id.activity_main_viewPager);
         mViewPager.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager()));
