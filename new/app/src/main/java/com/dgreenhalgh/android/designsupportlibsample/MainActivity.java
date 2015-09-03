abdef41d47bc3735c49058217f6f36fffc1de0a1
David Greenhalgh <davidrgreenhalgh@gmail.com>
2015-09-03 16:31:29 -0400

    Added percent demo to new sample

--- new/app/src/main/java/com/dgreenhalgh/android/designsupportlibsample/MainActivity.java
+++ new/app/src/main/java/com/dgreenhalgh/android/designsupportlibsample/MainActivity.java
@@ -1,17 +1,20 @@
 package com.dgreenhalgh.android.designsupportlibsample;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
     private TabLayout mTabLayout;
     private ViewPager mViewPager;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +29,20 @@ public class MainActivity extends AppCompatActivity {
 
         mTabLayout = (TabLayout) findViewById(R.id.activity_main_tabLayout);
         mTabLayout.setupWithViewPager(mViewPager);
+
+        mNavigationView = (NavigationView) findViewById(R.id.activity_main_navigationView);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.drawer_item_second:
+                        Intent intent  = PercentDemoActivity.newIntent(mNavigationView.getContext());
+                        startActivity(intent);
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
---
+++ new/app/src/main/java/com/dgreenhalgh/android/designsupportlibsample/PercentDemoActivity.java
@@ -0,0 +1,19 @@
+package com.dgreenhalgh.android.designsupportlibsample;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PercentDemoActivity extends AppCompatActivity {
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PercentDemoActivity.class);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_percent_demo);
+    }
+}
