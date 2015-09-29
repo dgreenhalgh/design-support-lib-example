3d7f3c7dfb0038ca2fa4780d903e4c9f79924f64
David Greenhalgh <davidrgreenhalgh@gmail.com>
2015-09-29 18:18:06 -0400

    Added a splash of color

--- new/app/src/main/java/com/dgreenhalgh/android/designsupportlibsample/FloatingActionButtonDemoActivity.java
+++ new/app/src/main/java/com/dgreenhalgh/android/designsupportlibsample/FloatingActionButtonDemoActivity.java
@@ -2,10 +2,15 @@ package com.dgreenhalgh.android.designsupportlibsample;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class FloatingActionButtonDemoActivity extends AppCompatActivity {
+
+    private FloatingActionButton mFloatingActionButton;
 
     public static Intent newIntent(Context context) {
         return new Intent(context, FloatingActionButtonDemoActivity.class);
@@ -15,5 +20,13 @@ public class FloatingActionButtonDemoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floating_action_button_demo);
+
+        mFloatingActionButton = (FloatingActionButton) findViewById(R.id.activity_floating_action_button_demo_floatingActionButton);
+        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Calling 411", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
