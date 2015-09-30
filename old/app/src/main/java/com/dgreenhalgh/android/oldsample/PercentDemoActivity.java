0e41957375740c63418947d45939ff0ec5cbd704
David Greenhalgh <davidrgreenhalgh@gmail.com>
2015-09-30 15:28:03 -0400

    Added layout_weight sample

---
+++ old/app/src/main/java/com/dgreenhalgh/android/oldsample/PercentDemoActivity.java
@@ -0,0 +1,15 @@
+package com.dgreenhalgh.android.oldsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PercentDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_percent_demo);
+    }
+}
