0634fd891193d72927b08283be97c423efea6daf
David Greenhalgh <davidrgreenhalgh@gmail.com>
2015-10-01 13:43:20 -0400

    Added MaterialEditText example

---
+++ old/app/src/main/java/com/dgreenhalgh/android/oldsample/TextInputDemoActivity.java
@@ -0,0 +1,13 @@
+package com.dgreenhalgh.android.oldsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextInputDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_input_demo);
+    }
+}
